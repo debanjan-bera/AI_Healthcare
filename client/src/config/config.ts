b8e1f6e673cd46b25ee94c615ada4bf286593b2d
debanjan-bera <debanjan.bera25discord@gmail.com>
2026-05-11 21:13:07 +0530

    feat: initialize client configuration and add gitignore file

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_END_POINT = import.meta.env.VITE_BACKEND_END_POINT || "http://localhost:8080/api/";
+export const BACKEND_END_POINT = import.meta.env.VITE_BACKEND_END_POINT || "https://ai-healthcare-1-ptja.onrender.com/api/";
 export const headers = {
   "Content-Type": "application/json",
 };
