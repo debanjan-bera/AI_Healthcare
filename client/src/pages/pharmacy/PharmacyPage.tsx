6daff5d27edd122b1628c74a9a476d1ebd890d15
debanjan-bera <debanjan.bera25discord@gmail.com>
2026-05-13 00:33:24 +0530

    refactor: remove unused React import from PharmacyPage component

--- client/src/pages/pharmacy/PharmacyPage.tsx
+++ client/src/pages/pharmacy/PharmacyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Search, ShoppingCart, Pill, Shield, ArrowRight, Plus, X } from 'lucide-react';
 import ThemeToggle from '../../components/ThemeToggle';
 import { useNavigate } from 'react-router-dom';
